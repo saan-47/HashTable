4d7cdaa7448326426dbb4f99a7bc5903c07b9aa3
Hassan <syed.hassan.ibrahim53@gmail.com>
2021-06-01 00:49:54 +0500

    HashTable

---
+++ src/hash/Entry.java
@@ -0,0 +1,23 @@
+package hash;
+
+public class Entry {
+    int key;
+    Object value;
+    Entry next;
+    public Entry(int key, Object value) {
+        this.key = key;
+        this.value = value;
+        next = null;
+    }
+
+    public Entry() {
+        next = null;
+    }
+
+    public int getKey(){
+        return key;
+    }
+    public Object getObject(){
+        return value;
+    }
+}
